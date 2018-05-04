67360643006d22c39e6d222bf9d348570db61fdc
Derek Ruths <derek@derekruths.com>
2018-05-03 23:17:18 -0500

    Finally got this thing working - initial conditions are a bear

--- src/rezin.py
+++ src/rezin.py
@@ -45,9 +45,7 @@ def logging_level():
     return logger.getEffectiveLevel()
 
 
-valid_context_attrs = ['memA','i0','H','W','L']
-
-# X is context: memA,i0,H,W,L,rho
+VALID_CONTEXT_ATTRS = ['memA','i0','H','W','L']
 class Context:
     pass
 
@@ -68,13 +66,7 @@ def load_location_history(fname):
     return H
 
 def compute_W(H):
-    W = []
-    acc = 0
-    for i in range(len(H)):
-        acc += H[i][1]
-        W.append(acc)
-
-    return W
+    return [h[1] for h in H]
 
 def compute_i0(W,rho):
     for i in range(len(W)):
@@ -84,12 +76,20 @@ def compute_i0(W,rho):
     # couldn't find an i0!  The history isn't long enough
     return None
 
+def compute_iS(W,rho):
+    for i in range(1,len(W)):
+        if sum(W[1:(i+1)]) >= rho:
+            return i
+
+    # couldn't find an iS!  The history isn't long enough - just use the base case.
+    return None
+
 def compute_iF(W,i0,rho):
     for i in range(i0+1,len(W)):
         if sum(W[i:]) < rho:
-            return i
+            return i-1
 
-    return i0
+    return len(W)-1
 
 def compute_L(H):
     return set([h[0] for h in H])
@@ -97,9 +97,10 @@ def compute_L(H):
 ####
 # Core algorithm
 
-def initialize_A(H,L,i0):
+def initialize_A(H,L,i0,iS):
     memA = {}
 
+    # initialize the A(i0,l)
     for l in L:
         away_time = 0
         for i in range(0,i0+1):
@@ -108,38 +109,62 @@ def initialize_A(H,L,i0):
 
         memA[(i0,l)] = (away_time,[])
 
+    # initialize the A(i,l) for i0 < i < iS
+    for l in L:
+        for i in range(i0+1,iS):
+            away_time = memA[(i0,l)][0]
+            for j in range(i0+1,i+1):
+                if H[j][0] != l:
+                    away_time += H[j][1]
+
+            memA[(i,l)] = (away_time,[])
+
+    # done
     return memA
 
 def compute_Q(i,X):
 
-    for j in range(X.i0+1,i):
-        if sum(X.W[j:i]) <= X.rho:
-            return j-1
+    Q = []
+    for j in range(X.i0,i):
+        if sum(X.W[(j+1):(i+1)]) >= X.rho:
+            Q.append(j)
 
-    # if we couldn't find an index, start with i0
-    return X.i0
+    # if we couldn't find an index, there's an error
+    if len(Q) == 0:
+        raise Exception('No valid Q(%d) could be computed - i_s was incorrect or not checked?' % i)
+    else:
+        return Q
 
-def compute_A(i,l,X):
+def compute_A(i,l,X,debug=False):
 
     if (i,l) in X.memA:
         return X.memA[(i,l)][0]
      
+    if debug: print('\tA(%d,%s)' % (i,l))
+
     Q = compute_Q(i,X)
+    
+    if debug: print('\tQ=%s' % Q)
     
     min_prev_rezes = None
     min_total_cost = float('infinity')
-    for j in range(X.i0,Q+1):
+    for j in Q: #range(X.i0,Q+1):
         for loc in X.L:
+
             # compute time prior to living here
             prev_rez_cost = compute_A(j,loc,X)
 
             # compute time away while living here
             cur_rez_cost = 0
             for k in range(j+1,i+1):
+                if debug: print('k = %d' % k)
                 if X.H[k][0] != l:
+                    if debug: print('%s != %s' % (X.H[k][0],l))
                     cur_rez_cost += X.H[k][1]
 
             total_cost = prev_rez_cost + cur_rez_cost
+
+            if debug: print('\t(%d,%s) mid (%d,%s): total_cost %d = %d + %d' % (i,l,j,loc,total_cost,prev_rez_cost,cur_rez_cost))
 
             # keep track of the min total cost
             if total_cost < min_total_cost:
@@ -164,7 +189,7 @@ def compute_trailing_cost(l,X):
 
     return acc
 
-def reconstruct_residence_history(X):
+def reconstruct_residence_history(X,debug=False):
 
     ####
     # run the algorithm
@@ -172,28 +197,37 @@ def reconstruct_residence_history(X):
     min_away = float('infinity')
 
     # compute the cost of residence ending at location l at time iF
-    #if logging_level() == logging.DEBUG: print('Total away costs:')
+    if debug:
+        logging.debug('Total away time:')
+
     for l in X.L:
-        away_time = compute_A(X.iF,l,X) + compute_trailing_cost(l,X)
+        dyn_away_time = compute_A(X.iF,l,X,debug=debug)
+        trailing_away_time = compute_trailing_cost(l,X)
+        away_time = dyn_away_time + trailing_away_time 
+        if debug:
+            print('\t%s\t%d=%d+%d' % (l,away_time,dyn_away_time,trailing_away_time))
     
-        #if logging_level() == logging.DEBUG: print('\t%s = %d' % (l,away_time))
+        if debug:
+            print_move_history_tree(X.iF,l,X,2)
+
         if away_time < min_away:
             min_loc = l
             min_away = away_time
-
-    #if logging_level() == logging.DEBUG: print()
 
     ####
     # build the move history
     move_history = [(X.iF,min_loc)]
     while True:
+        #print('Move history part - ',move_history[0])
         cur_rez = move_history[0]
 
-        if cur_rez[0] == X.i0:
+        if cur_rez[0] < X.iS:
             break
         else:
             cur_rez = X.memA[cur_rez][1][0]
             move_history.insert(0,cur_rez)
+
+    #print('Move history: ',move_history)
 
     ####
     # make the residence history from the move history
@@ -202,12 +236,17 @@ def reconstruct_residence_history(X):
     while len(move_history) > 0:
         move = move_history.pop(0)
 
-        R.append((move[1],next_move_idx))
+        if len(R) > 0 and R[-1][0] == move[1]:
+            # detect staying in the same place ... that's not moving!
+            pass
+        else:
+            R.append((move[1],next_move_idx))
+
         next_move_idx = move[0]+1
 
     return R
 
-def rezin(H,rho):
+def rezin(H,rho,debug=False):
     """
     Function for running the entire algorithm on
         H the location history
@@ -220,11 +259,13 @@ def rezin(H,rho):
     L = compute_L(H)
     W = compute_W(H)
     i0 = compute_i0(W,rho)
+    iS = compute_iS(W,rho)
     iF = compute_iF(W,i0,rho)
 
     X = Context()
-    X.memA = initialize_A(H,L,i0) # for memoizing the results of the dynamic algorithm
+    X.memA = initialize_A(H,L,i0,iS) # for memoizing the results of the dynamic algorithm
     X.i0 = i0 # the index in the location history where we initialize the A's
+    X.iS = iS # the index where we begin computing the first A's (beyond those initialized)
     X.iF = iF # the last index in the location history our dynamic algorithm should evaluate
     X.H = H # the location history
     X.W = W # the stay times for each position in the location history - for efficiency
@@ -232,8 +273,17 @@ def rezin(H,rho):
     X.rho = rho # the min residence time interval
     X.n = len(H)-1 # the last index in the location history
 
+    logger.debug('Context')
+    logger.debug('\ti0 ' + str(i0))
+    logger.debug('\tiS ' + str(iS))
+    logger.debug('\tiF ' + str(iF))
+    logger.debug('\tH ' + str(H))
+    logger.debug('\tW ' + str(W))
+    logger.debug('\tL ' + str(L))
+    logger.debug('')
+
     # run the algorithm
-    rez_history = reconstruct_residence_history(X)
+    rez_history = reconstruct_residence_history(X,debug=debug)
 
     return rez_history
 
@@ -249,6 +299,20 @@ def print_move_history_tree(i,l,X,indent=0):
     else:
         for last_rez in X.memA[cur_rez][1]:
             print_move_history_tree(*last_rez,X,indent+1)
+
+def print_interleaved_histories(R,H):
+    localH = list(H)
+    localR = list(R)
+
+    idx = 0
+    while len(localH) > 0:
+        if len(localR) > 0 and localR[0][1] <= idx:
+            print(' => %s' % localR.pop(0)[0])
+        else:
+            print('%s\t%d' % localH.pop(0))
+            idx += 1
+
+    return
 
 ####
 # Main for testing this
@@ -270,6 +334,9 @@ def main():
 
     rez_history = rezin(H,rho)
     print('Residence history:',rez_history)
+
+    print('\nInterleaved histories:')
+    print_interleaved_histories(rez_history,H)
 
     return
 
